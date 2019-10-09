3941fee3252be26e45e44e3956d791e4416b7611
Marc Mason <iammarcmason@googlemail.com>
2019-10-09 22:41:21 +0100

    add burger

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -3,15 +3,15 @@
     <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
       <v-list>
         <template v-for="(item, index) in items">
-          <v-list-tile :key="index">
-            <v-list-tile-content>{{item.title}}</v-list-tile-content>
-          </v-list-tile>
+          <v-list-item :key="index">
+            <v-list-item-content>{{item.title}}</v-list-item-content>
+          </v-list-item>
           <v-divider :key="`divider-${index}`"></v-divider>
         </template>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app color="brown darken-4" dark>
-      <v-app-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-side-icon>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer class="hidden-md-and-up"></v-spacer>
       <v-toolbar-title>{{appTitle}}</v-toolbar-title>
       <v-btn text class="hidden-sm-and-down">Menu</v-btn>
