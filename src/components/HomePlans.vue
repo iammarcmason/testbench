e04a2809b1c84b5a3c579e044e753a26e7979fee
Marc Mason <iammarcmason@googlemail.com>
2019-10-10 15:40:53 +0100

    update text and images

--- src/components/HomePlans.vue
+++ src/components/HomePlans.vue
@@ -5,29 +5,31 @@
     </v-layout>
     <v-layout row wrap>
       <v-flex xs12 sm12 md4>
-        <v-card>
-          <v-img src="https://via.placeholder.com/500x600" height="500px">
-            <v-container fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <span class="headline white--text">PRJ1</span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-img>
+        <v-hover v-slot:default="{ hover }">
+          <v-card>
+            <v-img src="https://picsum.photos/id/1019/700/800" height="500px">
+              <v-container fill-height fluid>
+                <v-layout fill-height>
+                  <v-flex xs12 align-end flexbox>
+                    <span class="headline white--text">PRJ1</span>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-img>
 
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">Project 1</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
-            </div>
-          </v-card-title>
-        </v-card>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Project 1</h3>
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
+              </div>
+            </v-card-title>
+          </v-card>
+        </v-hover>
       </v-flex>
 
       <v-flex xs12 sm12 md4>
         <v-card>
-          <v-img src="https://via.placeholder.com/500x600" height="500px">
+          <v-img src="https://picsum.photos/id/1018/700/800" height="500px">
             <v-container fill-height fluid>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
@@ -47,7 +49,7 @@
 
       <v-flex xs12 sm12 md4>
         <v-card>
-          <v-img src="https://via.placeholder.com/500x600" height="500px">
+          <v-img src="https://picsum.photos/id/1022/700/800" height="500px">
             <v-container fill-height fluid>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
@@ -75,7 +77,7 @@ export default {
     disabled: false,
     openDelay: "0",
     closeDelay: "0",
-    value: false
+    value: true
   })
 };
 </script>
