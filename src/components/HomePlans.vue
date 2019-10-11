f7f988fe437bc3d4a92114a7f466662312bc88a0
Marc Mason <iammarcmason@googlemail.com>
2019-10-11 19:24:14 +0100

    include image in latest posts

--- src/components/HomePlans.vue
+++ src/components/HomePlans.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container grid-list-lg class="btm">
     <v-layout row>
-      <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Previous / Current Work</v-flex>
+      <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Latest posts</v-flex>
     </v-layout>
     <v-layout row wrap>
       <v-flex xs12 sm12 md4 v-for="post in posts" v-bind:key="post.fields.slug">
         <v-card>
-          <v-img src="https:" +{{post.fields.file.url}} height="500px">
+          <v-img :src="post.fields.heroImage.fields.file.url" height="500px">
             <v-container fill-height fluid>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
@@ -24,48 +24,6 @@
           </v-card-title>
         </v-card>
       </v-flex>
-      <!--
-      <v-flex xs12 sm12 md4>
-        <v-card>
-          <v-img src="https://picsum.photos/id/1018/700/800" height="500px">
-            <v-container fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <span class="headline white--text">PRJ2</span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-img>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">Project 2</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
-            </div>
-          </v-card-title>
-        </v-card>
-      </v-flex>
-
-      <v-flex xs12 sm12 md4>
-        <v-card>
-          <v-img src="https://picsum.photos/id/1022/700/800" height="500px">
-            <v-container fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <span class="headline white--text">PRJ3</span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-img>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">Project 3</h3>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.</div>
-            </div>
-          </v-card-title>
-        </v-card>
-      </v-flex>
-
-      -->
     </v-layout>
   </v-container>
 </template>
@@ -74,18 +32,20 @@
 const query = {
   skip: 0,
   limit: 3,
-  order: "-sys.createdAt"
+  order: "-sys.createdAt",
+  include: 3
 };
 
 export default {
   name: "HomePlans",
   data() {
-    return { posts: [] };
+    return { posts: [], includes: [] };
   },
   mounted() {
     return this.$contentful
       .getEntries(query)
       .then(res => (this.posts = res.items))
+      .then(res => (this.assets = res.includes))
       .catch(console.error);
   }
 };
