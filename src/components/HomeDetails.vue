253fdeeb7fbb36c9d36ddf30ca5edab27f129a02
Marc Mason <iammarcmason@googlemail.com>
2019-10-14 20:25:53 +0100

    update content types

--- src/components/HomeDetails.vue
+++ src/components/HomeDetails.vue
@@ -2,27 +2,31 @@
   <v-container>
     <v-layout column>
       <v-flex class="display-2 text-xs-center my-5">Welcome to my Stuff</v-flex>
-      <v-flex>
-        <div class="headline mt-3">Blog</div>
-        <p class="subheading mt-3">My blog posts will live here...</p>
-      </v-flex>
-      <v-flex>
-        <div class="headline mt-3">About</div>
-        <p
-          class="subheading mt-3"
-        >This will have something about who I am and what I do, possibly CV...</p>
-      </v-flex>
-      <v-flex>
-        <div class="headline mt-3">Projects</div>
-        <p class="subheading mt-3">Stuff I've done / am doing...</p>
+      <v-flex v-for="post in posts" v-bind:key="post.fields.slug">
+        <div class="headline mt-3">{{ post.fields.header }}Blog</div>
+        <p class="subheading mt-3">{{ post.fields.body }}My blog posts will live here...</p>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+const query = {
+  skip: 0,
+  limit: 10,
+  content_type_id: "blogPost",
+  order: "-sys.createdAt"
+};
 export default {
-  name: "HomeDetails"
+  name: "HomeDetails",
+  data() {
+    return { posts: [] };
+  },
+  mounted() {
+    return this.$contentful
+      .getEntries(query)
+      .then(res => (this.posts = res.items));
+  }
 };
 </script>
 
